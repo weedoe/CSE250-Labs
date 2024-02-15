5136a81da2d726ffbc4df2e963229d48f20ae147
William Paterson <weedoe@weedoe.ca>
2024-02-15 11:33:07 -0500

    Add new sorting algos and change cmakelists

--- Lab2/main.cpp
+++ Lab2/main.cpp
@@ -20,15 +20,18 @@ struct type_flights {
 
 //declaring functions
 int open_file(ifstream &flights, const string &file_name);
-int sort_flights(type_flights flights_array[], int n);
+int selection_sort_flights(type_flights flights_array[], int n);
 void firstlast_flights(type_flights flights_array[], int n);
+int merge_sort_flights(type_flights flights_array[], int L, int R);
+int quick_sort_flights(type_flights flights_array[], int L, int R);
 
 
 int main() {
     ifstream flights;
     string file_name = "flights_all_fixed.dat";
     int count = open_file(flights, file_name);
-    type_flights flights_array[count];
+    unsigned long total_time;
+    auto* flights_array = new type_flights[count];
     flights.open(file_name);
     if (!flights.is_open()) {
         cerr << "Error opening file" << endl;
@@ -46,12 +49,88 @@ int main() {
     }
     flights.close();
 
+    //Question 1
+    cout << "----------Question 1----------" << endl;
     firstlast_flights(flights_array, count); //print the first and last 5 flights
-
+    clock_t start = clock();
     //sort the flights by flight number
-    sort_flights(flights_array, count);
-
+    selection_sort_flights(flights_array, count);
+    total_time = clock() - start;
     firstlast_flights(flights_array, count); //print the first and last 5 flights
+    cout << "SELECTION sort took " << total_time /  CLOCKS_PER_SEC << " seconds" << endl << endl;
+
+    start = clock();
+    selection_sort_flights(flights_array, count); //sort the flights by flight number
+    total_time = clock() - start;
+    firstlast_flights(flights_array, count); //print the first and last 5 flights
+    cout << "SELECTION REsort took " << total_time /  CLOCKS_PER_SEC << " seconds" << endl << endl;
+
+    delete[] flights_array;
+
+    //Question 2
+    cout << "----------Question 2----------" << endl;
+    open_file(flights, file_name);
+    flights_array = new type_flights[count];
+    flights.open(file_name);
+    if (!flights.is_open()) {
+        cerr << "Error opening file" << endl;
+    }
+    for (i = 0; i < count; i++) { //read the file and store the values in the array
+        getline(flights, tmp);
+        stringstream ss(tmp);
+        getline(ss, flights_array[i].flight_number, ',');
+        getline(ss, flights_array[i].departure_city, ',');
+        getline(ss, flights_array[i].arrival_city, ',');
+        ss >> flights_array[i].flight_cost;
+    }
+    flights.close();
+    start = clock();
+    merge_sort_flights(flights_array, 0, count - 1);
+    total_time = clock() - start;
+    firstlast_flights(flights_array, count); //print the first and last 5 flights
+    cout << "MERGE sort took " << total_time /  CLOCKS_PER_SEC << " seconds" << endl << endl;
+
+    start = clock();
+    merge_sort_flights(flights_array, 0, count - 1);
+    total_time = clock() - start;
+    firstlast_flights(flights_array, count); //print the first and last 5 flights
+    cout << "MERGE REsort took " << total_time /  CLOCKS_PER_SEC << " seconds" << endl << endl;
+
+    delete[] flights_array;
+
+    //Question 3
+    cout << "----------Question 3----------" << endl;
+    open_file(flights, file_name);
+    flights_array = new type_flights[count];
+    flights.open(file_name);
+    if (!flights.is_open()) {
+        cerr << "Error opening file" << endl;
+    }
+    for (i = 0; i < count; i++) { //read the file and store the values in the array
+        getline(flights, tmp);
+        stringstream ss(tmp);
+        getline(ss, flights_array[i].flight_number, ',');
+        getline(ss, flights_array[i].departure_city, ',');
+        getline(ss, flights_array[i].arrival_city, ',');
+        ss >> flights_array[i].flight_cost;
+    }
+    flights.close();
+    start = clock();
+    quick_sort_flights(flights_array, 0, count - 1);
+    total_time = clock() - start;
+    firstlast_flights(flights_array, count); //print the first and last 5 flights
+    cout << "QUICK sort took " << total_time /  CLOCKS_PER_SEC << " seconds" << endl << endl;
+
+    start = clock();
+    quick_sort_flights(flights_array, 0, count - 1);
+    total_time = clock() - start;
+    firstlast_flights(flights_array, count); //print the first and last 5 flights
+    cout << "QUICK REsort took " << total_time /  CLOCKS_PER_SEC << " seconds" << endl << endl;
+
+    delete[] flights_array;
+
+
+
     return 0;
 }
 
@@ -74,7 +153,7 @@ int open_file(ifstream &flights, const string &file_name) {
 }
 
 //function to sort the flights by flight number
-int sort_flights(type_flights flights_array[], int n) {
+int selection_sort_flights(type_flights flights_array[], int n) {
     for (int i = 0; i < n - 1; i++) {
         int min = i;
         for (int j = i + 1; j < n; j++) {
@@ -105,4 +184,78 @@ void firstlast_flights(type_flights flights_array[], int n) {
              << flights_array[i].flight_cost << endl;
     }
     cout << endl;
+}
+int merge_sort_flights(type_flights flights_array[], int L, int R) {
+    if (L < R) {
+        int M = (L + R) / 2;
+        merge_sort_flights(flights_array, L, M);
+        merge_sort_flights(flights_array, M + 1, R);
+        int i, j, k;
+        int n1 = M - L + 1;
+        int n2 = R - M;
+
+        auto* L_arr = new type_flights[n1];
+        auto* R_arr = new type_flights[n2];
+
+        for (i = 0; i < n1; i++) {
+            L_arr[i] = flights_array[L + i];
+        }
+        for (j = 0; j < n2; j++) {
+            R_arr[j] = flights_array[M + 1 + j];
+        }
+        i = 0;
+        j = 0;
+        k = L;
+        while (i < n1 && j < n2) {
+            if (L_arr[i].flight_number <= R_arr[j].flight_number) {
+                flights_array[k] = L_arr[i];
+                i++;
+            } else {
+                flights_array[k] = R_arr[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < n1) {
+            flights_array[k] = L_arr[i];
+            i++;
+            k++;
+        }
+        while (j < n2) {
+            flights_array[k] = R_arr[j];
+            j++;
+            k++;
+        }
+
+        delete[] L_arr;
+        delete[] R_arr;
+
+    }
+    return 0;
+}
+int quick_sort_flights(type_flights flights_array[], int L, int R) {
+    if (L < R) {
+        int pivot = L;
+        int i = L;
+        int j = R;
+        while (i < j) {
+            while (flights_array[i].flight_number <= flights_array[pivot].flight_number && i < R) {
+                i++;
+            }
+            while (flights_array[j].flight_number > flights_array[pivot].flight_number) {
+                j--;
+            }
+            if (i < j) {
+                type_flights temp = flights_array[i];
+                flights_array[i] = flights_array[j];
+                flights_array[j] = temp;
+            }
+        }
+        type_flights temp = flights_array[pivot];
+        flights_array[pivot] = flights_array[j];
+        flights_array[j] = temp;
+        quick_sort_flights(flights_array, L, j - 1);
+        quick_sort_flights(flights_array, j + 1, R);
+    }
+    return 0;
 }
