b783b26024ad2ce4b231fdf1fcd991991710f99e
William <weedoe@weedoe.ca>
2024-04-04 20:01:14 -0400

    reformat

--- Lab5/Network.cpp
+++ Lab5/Network.cpp
@@ -18,9 +18,9 @@ using namespace std;
 
 /** This method colors all vertices in the graph WHITE. */
 void Network::whiten() {
-	for (int i=0 ;i<m_NetworkSize; i++){
-		m_Colors[i] = WHITE;
-	}
+    for (int i = 0; i < m_NetworkSize; i++) {
+        m_Colors[i] = WHITE;
+    }
 }
 
 
@@ -29,37 +29,37 @@ void Network::whiten() {
  * @param IPList	Array of strings of each server's IP
  *
   */
-Network::Network(const vector<string>& IPList) {
-	//Debug statement
-	cout << "Building a network of " << IPList.size() << " servers" << endl;
-	//Fix the network size
-	m_NetworkSize = IPList.size();
-	//Saves the server IPs
-	m_Vertex2IP = IPList;
-	//Allocate space for adjacency lists and colors
-	//By default there are no connections, so all nullptr
-	m_EdgeLists = new EdgeListNode*[m_NetworkSize];
-	for (int i=0; i<IPList.size(); i++) {
-		m_EdgeLists[i] = nullptr;
-	}
-	m_Colors = new color[m_NetworkSize];
-	//Fill a hash table for fast search using IP
-	for (int i=0; i<IPList.size(); i++) {
-		m_IP2Vertex.insert({IPList[i], i});
-	}
-	//Color the graph white
-	whiten();
+Network::Network(const vector<string> &IPList) {
+    //Debug statement
+    cout << "Building a network of " << IPList.size() << " servers" << endl;
+    //Fix the network size
+    m_NetworkSize = IPList.size();
+    //Saves the server IPs
+    m_Vertex2IP = IPList;
+    //Allocate space for adjacency lists and colors
+    //By default there are no connections, so all nullptr
+    m_EdgeLists = new EdgeListNode *[m_NetworkSize];
+    for (int i = 0; i < IPList.size(); i++) {
+        m_EdgeLists[i] = nullptr;
+    }
+    m_Colors = new color[m_NetworkSize];
+    //Fill a hash table for fast search using IP
+    for (int i = 0; i < IPList.size(); i++) {
+        m_IP2Vertex.insert({IPList[i], i});
+    }
+    //Color the graph white
+    whiten();
 }
 
 /** Frees up all allocated memory. */
 Network::~Network() {
-	delete[] m_EdgeLists;
-	delete[] m_Colors;
+    delete[] m_EdgeLists;
+    delete[] m_Colors;
 }
 
 /** Number of servers in the network */
-int Network::GetNetworkSize(){
-	return m_NetworkSize;
+int Network::GetNetworkSize() {
+    return m_NetworkSize;
 }
 
 /** This adds a connection between two computers based on their IPs
@@ -70,25 +70,25 @@ int Network::GetNetworkSize(){
  * @param server1	IP of the server at one end
  * @param server2	IP of the server at the other end
  */
-void Network::AddConnection(const string& server1, const string& server2){
-	//Extract servers index from IPs
-	int v1 =  m_IP2Vertex[server1];
-	int v2 =  m_IP2Vertex[server2];
-	//Debug statement
-	cout << "Adding edge (" << server1 << "," << server2 << ")" << endl;
-	//Add the edge v1 to s2
-	auto edge1 = new EdgeListNode();
-	edge1->v1 = v1;
-	edge1->v2 = v2;
-	edge1->next = m_EdgeLists[v1];
-	m_EdgeLists[v1] = edge1;
-	//Add the edge v2 to v1
-	//Note that since this is in the adjacency list of v2, we flip the vs
-	auto edge2 = new EdgeListNode();
-	edge2->v1 = v2;//see!
-	edge2->v2 = v1;//so the index adjacent to v2 is v1
-	edge2->next = m_EdgeLists[v2];
-	m_EdgeLists[v2] = edge2;
+void Network::AddConnection(const string &server1, const string &server2) {
+    //Extract servers index from IPs
+    int v1 = m_IP2Vertex[server1];
+    int v2 = m_IP2Vertex[server2];
+    //Debug statement
+    cout << "Adding edge (" << server1 << "," << server2 << ")" << endl;
+    //Add the edge v1 to s2
+    auto edge1 = new EdgeListNode();
+    edge1->v1 = v1;
+    edge1->v2 = v2;
+    edge1->next = m_EdgeLists[v1];
+    m_EdgeLists[v1] = edge1;
+    //Add the edge v2 to v1
+    //Note that since this is in the adjacency list of v2, we flip the vs
+    auto edge2 = new EdgeListNode();
+    edge2->v1 = v2;//see!
+    edge2->v2 = v1;//so the index adjacent to v2 is v1
+    edge2->next = m_EdgeLists[v2];
+    m_EdgeLists[v2] = edge2;
 }
 
 /** This method must search the graph, starting on the homeServer
@@ -105,21 +105,21 @@ void Network::AddConnection(const string& server1, const string& server2){
  * @return a C++ string as described above
  */
 
-string Network::FindShortestPath(const string& homeServer, const string& targetServer) {
-	//You can use a queue... for breadth first search
-	queue<int> myqueue;
-	//You can use a stack... if you need to ;-)
-	stack<string> mystack;
-	//Don't forget to whiten the graph
-	whiten();
-	//I suggest you first convert the IP strings back into indexes
-	int home = m_IP2Vertex[homeServer];
-	int destination = m_IP2Vertex[targetServer];
-	//Debug statement
-	cout << "Finding fastest path from  " << home << " to " << destination << endl;
-    int* distances = new int[GetNetworkSize()]; //remember to delete these at the end
-	int* parents = new int[GetNetworkSize()];
-    for (int i=0; i<m_NetworkSize; i++) { //initialize distances and parents
+string Network::FindShortestPath(const string &homeServer, const string &targetServer) {
+    //You can use a queue... for breadth first search
+    queue<int> myqueue;
+    //You can use a stack... if you need to ;-)
+    stack<string> mystack;
+    //Don't forget to whiten the graph
+    whiten();
+    //I suggest you first convert the IP strings back into indexes
+    int home = m_IP2Vertex[homeServer];
+    int destination = m_IP2Vertex[targetServer];
+    //Debug statement
+    cout << "Finding fastest path from  " << home << " to " << destination << endl;
+    int *distances = new int[GetNetworkSize()]; //remember to delete these at the end
+    int *parents = new int[GetNetworkSize()];
+    for (int i = 0; i < m_NetworkSize; i++) { //initialize distances and parents
         distances[i] = -1;
         parents[i] = -2;
     }
@@ -128,7 +128,7 @@ string Network::FindShortestPath(const string& homeServer, const string& targetS
     while (!myqueue.empty()) {
         int current = myqueue.front(); //get the front of the queue
         myqueue.pop();
-        EdgeListNode* edge = m_EdgeLists[current]; //get the edge list of the current server
+        EdgeListNode *edge = m_EdgeLists[current]; //get the edge list of the current server
         while (edge != nullptr) { //while the edge is not null
             if (m_Colors[edge->v2] == WHITE) { //if the color of the destination server is white
                 m_Colors[edge->v2] = GRAY; //change the color to gray
